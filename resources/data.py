3d310618a9dcdc4167d8c6ea6b8d9083f2d71d90
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-08-21 13:26:35 +0700

    Robot POM - Add Data as Resources

---
+++ resources/data.py
@@ -0,0 +1,9 @@
+# GENERAL
+base_url="https://reqres.in/api"
+
+# USER
+user_id= "2"
+user_default_mail= "janet.weaver@reqres.in"
+user_new_mail = "eve.holt@reqres.in"
+user_password= "12345"
+user_new_password= "54321"
