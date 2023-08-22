10d6bac3347c0f1442964fea9427b001faaaf865
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-08-22 14:34:11 +0700

    Robot POM - Remove unused data

--- resources/data.py
+++ resources/data.py
@@ -10,6 +10,5 @@ user_job = "Lead QA Automation"
 user_default_mail= "janet.weaver@reqres.in"
 user_new_mail = "eve.holt@reqres.in"
 user_password= "123456"
-user_new_password= "654321"
 user_new_job = "Lead Engineers"
 user_list = ["Michael", "Lindsay", "Tobias", "Byron", "George", "Rachel"]
